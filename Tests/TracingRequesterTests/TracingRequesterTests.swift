c62f18247875b870d1c4d8a7ff30c8e5ad0c8825
Dmitry <dmitry@aleksandrs-mbp.kpn>
2024-05-19 16:52:40 +0200

    Adds Sourcery

--- Tests/TracingRequesterTests/TracingRequesterTests.swift
+++ Tests/TracingRequesterTests/TracingRequesterTests.swift
@@ -1,12 +1,21 @@
+import Foundation
 import XCTest
 @testable import TracingRequester
 
 final class TracingRequesterTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    private var subject: Requester!
+    
+    override func setUp() async throws {
+        try await super.setUp()
+        
+    }
+    
+    override func tearDown() async throws {
+        try await super.tearDown()
+        
+    }
+    
+    func testLowLoad() throws {
+        
     }
 }
